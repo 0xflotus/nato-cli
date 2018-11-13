4beb1f00261dae688ba2fe4361054caae7e3afaf
0xflotus <0xflotus@gmail.com>
2018-11-13 18:17:24 +0100

    new config

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-const nato = require("nato");
+const nato = require("nato")
 
 interface ILetter {
-    text: string;
-    pronunciation: string;
+    text: string
+    pronunciation: string
 }
 
 process.argv.slice(2).forEach((val: string, index: number) => {
@@ -10,5 +10,5 @@ process.argv.slice(2).forEach((val: string, index: number) => {
         new nato(val.toLowerCase()).natified
             .map((e: ILetter) => e.text)
             .join(" ")
-    );
-});
+    )
+})
