eab81c2e14d8250b39851a33d1ef9662a0d2fa96
0xflotus <0xflotus@gmail.com>
2018-11-14 02:12:19 +0100

    added guard

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { dict1 } from "./dictionary"
 
 process.argv.slice(2).forEach((val: string) => {
   console.log(
-    val
-      .split("")
-      .map((e: string) => dict1.get(e.toLowerCase())![0])
-      .join(" ")
+    /[^a-zA-Z]/.test(val)
+      ? `There are not supported characters in '${val}'`
+      : val
+          .split("")
+          .map((e: string) => dict1.get(e.toLowerCase())![0])
+          .join(" ")
   )
 })
