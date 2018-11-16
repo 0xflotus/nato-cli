7982066b32dce81349edb3b6cdeb3b5f1e943c0d
0xflotus <0xflotus@gmail.com>
2018-11-16 03:54:05 +0100

    chalk support

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { dict1 } from "./dictionary"
 const pjson = require("pjson")
 const osLocale = require("os-locale")
+const chalk = require("chalk")
 
 if (["-v", "--version", "version"].indexOf(process.argv[2]) > -1) {
   console.log(pjson.version)
@@ -8,11 +9,13 @@ if (["-v", "--version", "version"].indexOf(process.argv[2]) > -1) {
   process.argv.slice(2).forEach((val: string) => {
     console.log(
       /[^a-zA-Z]/.test(val)
-        ? `${
-            osLocale.sync() === "de_DE"
-              ? "Es gibt nicht unterstuetzte Zeichen in"
-              : "There are not supported characters in"
-          } '${val}'`
+        ? chalk.red.bold(
+            `${
+              osLocale.sync() === "de_DE"
+                ? "Es gibt nicht unterstuetzte Zeichen in"
+                : "There are not supported characters in"
+            } '${val}'`
+          )
         : val
             .split("")
             .map((e: string) => dict1.get(e.toLowerCase())![0])
