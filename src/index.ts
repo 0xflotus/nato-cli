d0f878eae0499183503ac444303a8642e17ad844
0xflotus <0xflotus@gmail.com>
2018-11-16 03:37:53 +0100

    i18n support

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { dict1 } from "./dictionary"
 const pjson = require("pjson")
+const osLocale = require("os-locale")
 
 if (["-v", "--version", "version"].indexOf(process.argv[2]) > -1) {
   console.log(pjson.version)
@@ -7,7 +8,11 @@ if (["-v", "--version", "version"].indexOf(process.argv[2]) > -1) {
   process.argv.slice(2).forEach((val: string) => {
     console.log(
       /[^a-zA-Z]/.test(val)
-        ? `There are not supported characters in '${val}'`
+        ? `${
+            osLocale.sync() === "de_DE"
+              ? "Es gibt nicht unterstuetzte Zeichen in"
+              : "There are not supported characters in"
+          } '${val}'`
         : val
             .split("")
             .map((e: string) => dict1.get(e.toLowerCase())![0])
